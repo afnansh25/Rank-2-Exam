d9effc072b19cbdad5b2ae232d10715e91035090
afnansh25 <afnan.shaheen.5@gmail.com>
2025-05-13 14:41:35 +0000

    all levels completed

---
+++ Rank 2 Exam/Level 4/ft_split/ft_split.c
@@ -0,0 +1,52 @@
+
+#include <stdlib.h>
+
+char **ft_split(char *str)
+{
+    int i = 0;
+    int k = 0;
+    int start = 0;
+    int end = 0;
+    char **split = (char **)malloc(sizeof (char *) * 5000);
+    if (!split)
+        return (NULL);
+    while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+        i++;
+    while (str[i])
+    {
+        start = i;
+        while (str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
+            i++;
+        end = i;
+        split[k] = (char *)malloc(sizeof (char) * (end - start + 1));
+        if (!split)
+            return (NULL);
+        int j = 0;
+        while(start < end)
+        {
+            split[k][j] = str[start];
+            j++;
+            start++;
+        }
+        while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+            i++;
+        split[k][j] = '\0';
+        k++;
+    }
+    split[k] = NULL;
+    return (split);
+}
+
+#include <stdio.h>
+int main (void)
+{
+    char *str = "hello, my name is \t Afnan\n";
+    char **split = ft_split(str);
+    int i = 0;
+    while (split[i])
+    {
+        printf("%s\n", split[i]);
+        i++;
+    }
+    return (0);
+}
